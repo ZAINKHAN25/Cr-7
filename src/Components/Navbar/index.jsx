24bf4eaad77b352207710bf8bbb97f5206bbbbd3
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-04-04 16:56:10 +0500

    Navbar Work Done Without Href Work

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -3,8 +3,23 @@ import "./app.css"
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid px-5">
+        <a className="logoOfNavbar" href="#">Cristiano-Ronaldo</a>
+        <div className="BtnOfToogleNavbar" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <i class="fa-solid fa-bars"></i>
+        </div>
+        <div className="collapse navbar-collapse navbarLis" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link" href="#about">About</a>
+            <a className="nav-link" href="#clubs">Clubs</a>
+            <a className="nav-link" href="#achiements">Achiements</a>
+            <a className="nav-link" href="#pictures">Pictures</a>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
