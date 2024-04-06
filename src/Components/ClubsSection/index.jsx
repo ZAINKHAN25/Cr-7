b1f62ddb7b418d2cdb11cef1caf5716edade2711
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-04-06 21:38:58 +0500

    Clubs Section Almost Done

--- src/Components/ClubsSection/index.jsx
+++ src/Components/ClubsSection/index.jsx
@@ -9,13 +9,13 @@ function ClubsSection() {
             <h2 className='h2OfClubsSection'>Clubs:</h2>
 
             {/* Current Teams */}
-            <div className='currentTeamsLine mb-5'>
+            <div className='currentTeamsLine mb-3'>
                 {
                     currentTeams.map(
                         (x, i) => {
                             return (
-                                <div className='currentTeamsDiv mx-5' key={i}>
-                                    <img src={x?.imgLogo} alt="" />
+                                <div className='currentTeamsDiv m-5 px-2 py-4' key={i}>
+                                    <img className='mb-3' src={x?.imgLogo} alt="" />
                                     <h2 className='headingOfCurrentTeams'>{x?.teamName}</h2>
                                     <p className='mt-3 fs-4'>{x?.dateOfTeam}</p>
                                 </div>
@@ -24,21 +24,23 @@ function ClubsSection() {
                     )
                 }
             </div>
-            {/* Previos Teams */}
-            {
-                previosTeams.map(
-                    (x, i) => {
-                        return (
-                            <div className='previosTeamsDiv' key={i}>
-                                <img src={x?.imgLogo} alt="" />
-                                <h2>{x?.teamName}</h2>
-                                <p>{x?.dateOfTeam}</p>
-                            </div>
-                        )
-                    }
-                )
-            }
 
+            {/* Previos Teams */}
+            <div className='previousTeamsLine mb-5'>
+                {
+                    previosTeams.map(
+                        (x, i) => {
+                            return (
+                                <div className='previosTeamsDiv mx-3 my-3 px-2 py-4' key={i}>
+                                    <img src={x?.imgLogo} alt="" />
+                                    <h2 className='headingOfPreviosTeams'>{x?.teamName}</h2>
+                                    <p className='mt-3 fs-4'>{x?.dateOfTeam}</p>
+                                </div>
+                            )
+                        }
+                    )
+                }
+            </div>
 
         </div>
     )
